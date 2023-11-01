354392585b7180459048ac1bcac128b34d16617b
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-01 13:19:02 +0200

    Add my basic server

---
+++ Intro to Node.js/exercies/my-basic-server/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    if(url.pathname == '/'){
+        res.write('Home page');
+        res.end();
+    }else if(url.pathname == '/cats'){
+        res.write('Cats');
+        res.end();
+    }else{
+        res.statusCode = 404;
+        res.write('Not Found');
+        res.end;
+    }
+})
+
+server.listen(3000);
