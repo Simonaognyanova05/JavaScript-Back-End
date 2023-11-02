7bae82b811aa540aea8d44c8b01d9a8371c8cdaf
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-02 14:32:31 +0200

    Add demo-7

---
+++ Node.js Streams and Utilities/demo-7-with-html/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    console.log('Request');
+    if (req.method == 'GET') {
+       fs.createReadStream('index.html').pipe(res);
+    } else if (req.method == 'POST') {
+        let body = '';
+        req.on('data', data => {
+            console.log('Chunk >>> ', data.toString());
+            body += data;
+        });
+        req.on('end', () => {
+            const bodyAsObject = JSON.parse(body);
+            console.log('Body:', bodyAsObject);
+            bodyAsObject.price++;
+            res.writeHead(200, {
+                'content-type': 'application/json'
+            })
+            res.write(JSON.stringify(bodyAsObject));
+            res.end();
+        })
+    }
+}).listen(3000);
