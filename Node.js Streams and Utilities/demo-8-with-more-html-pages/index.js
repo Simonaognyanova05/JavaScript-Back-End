a82622957acf6a5d947e802a5866fbb7b992ef3f
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-03 12:28:40 +0200

    Add style to demo-8

--- Node.js Streams and Utilities/demo-8-with-more-html-pages/index.js
+++ Node.js Streams and Utilities/demo-8-with-more-html-pages/index.js
@@ -5,8 +5,13 @@ http.createServer((req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
 
     if(url.pathname == '/'){
-        fs.createReadStream(`./static/index.html`).pipe(res);
-    }else if(url.pathname.slice(-5) == '.html'){
+        res.writeHead(301, {
+            location: '/index.html',
+        })
+        res.end();
+    }else if(url.pathname.slice(-5) == '.html' || url.pathname.slice(-4) == '.css'){
         fs.createReadStream(`./static${url.pathname}`).pipe(res);
+    } else if(url.pathname == '/favicon.ico'){
+        fs.createReadStream(`./static/favicon.ico`).pipe(res);
     } 
 }).listen(3000);
