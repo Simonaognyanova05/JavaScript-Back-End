1a46e8ed9dfbeb6558d5a7fd4748d25426bfeba4
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-07 08:19:13 +0200

    Add catalog page

--- ExpressJS and Templating/my-exersice/index.js
+++ ExpressJS and Templating/my-exersice/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/catalog', (req, res) => {
+    res.sendFile(__dirname + '/catalog.html');
 })
 
 app.listen(3000);
