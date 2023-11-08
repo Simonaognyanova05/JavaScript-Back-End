ae3fef240b8859fe4cb601691963934cf846c445
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-08 21:17:29 +0200

    Add helpers to demo-8

--- ExpressJS and Templating/demo-8-templates/index.js
+++ ExpressJS and Templating/demo-8-templates/index.js
@@ -8,7 +8,12 @@ const hbs = handlebars.create({
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
+let visitors = 0;
+
 app.get('/', (req, res) => {
+    res.locals = {
+        count: visitors++
+    }
     res.render('home', { layout: false });
 })
 app.listen(3001);
