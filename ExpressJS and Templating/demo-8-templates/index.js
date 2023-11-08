527848d5467d8e3c76a90fd5b6737349b55c8148
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-08 21:24:07 +0200

    Add iteration to demo-8

--- ExpressJS and Templating/demo-8-templates/index.js
+++ ExpressJS and Templating/demo-8-templates/index.js
@@ -10,9 +10,16 @@ app.set('view engine', '.hbs');
 
 let visitors = 0;
 
+const products = [
+    {name: "Product 1", price: 222},
+    {name: "Product 2", price: 345},
+
+]
+
 app.get('/', (req, res) => {
     res.locals = {
-        count: visitors++
+        count: visitors++,
+        products
     }
     res.render('home', { layout: false });
 })
