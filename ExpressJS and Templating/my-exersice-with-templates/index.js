bbaaccde32bd4c3774c0ba0c2d3f21fbc9b5af63
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-10 20:01:01 +0200

    Add static page

--- ExpressJS and Templating/my-exersice-with-templates/index.js
+++ ExpressJS and Templating/my-exersice-with-templates/index.js
@@ -1,13 +1,15 @@
-const app = require('express')();
+const express = require('express');
 const hbs = require('express-handlebars');
 const homeController = require('./src/home');
 const catalogController = require('./src/cats');
 
+const app = express();
 app.engine('.hbs', hbs.create({
     extname: '.hbs'
 }).engine);
 
 app.set('view engine', '.hbs');
+app.use('/content', express.static('static'));
 app.get('/', homeController);
 app.get('/cats', catalogController);
 
