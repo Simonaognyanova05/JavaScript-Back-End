1afe037507e8f72921914fe8681d9b865bd2bd31
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-11 20:29:27 +0200

    Add home page with template

---
+++ Workshop-ExpressJS and Templating/Health-IT-with-templates/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const homeController = require('./src/home');
+
+const app = express();
+
+const hbs = handlebars.create({
+    extname: '.hbs'
+})
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.use('/content', express.static('static'));
+app.get('/', homeController);
+
+app.listen(3001);
---
+++ Workshop-ExpressJS and Templating/Health-IT-with-templates/src/home.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+    res.render('home', {
+        title: 'Home page', 
+        heading: 'Начало',
+        image: '<div class="image"><img src="/content/images/image2.png" alt="The beginning of site" class="firstImage"> </div>'
+    });
+}
