b7ec9c7ccb53361075bb418947d56b0588eb6602
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-11 20:42:36 +0200

    Add healthIt

--- Workshop-ExpressJS and Templating/Health-IT-with-templates/index.js
+++ Workshop-ExpressJS and Templating/Health-IT-with-templates/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const homeController = require('./src/home');
+const healthITController = require('./src/healthIT');
+
 
 const app = express();
 
@@ -12,5 +14,7 @@ app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 app.use('/content', express.static('static'));
 app.get('/', homeController);
+app.get('/healthIT', healthITController);
+
 
 app.listen(3001);
