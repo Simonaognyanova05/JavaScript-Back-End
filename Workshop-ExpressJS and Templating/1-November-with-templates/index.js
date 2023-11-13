9dd1cf2a5b8e957f1223ebe5b8f61ee67d897ee7
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-13 13:53:39 +0200

    Add paisii

--- Workshop-ExpressJS and Templating/1-November-with-templates/index.js
+++ Workshop-ExpressJS and Templating/1-November-with-templates/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const homeController = require('./src/home');
+const paisiiController = require('./src/Paisii');
+
 
 const app = express();
 const hbs = handlebars.create({
@@ -11,5 +13,7 @@ app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 app.use('/content', express.static('static'));
 app.get('/', homeController);
+app.get('/paisii', paisiiController);
+
 
 app.listen(3000);
---
+++ Workshop-ExpressJS and Templating/1-November-with-templates/src/Paisii.js
@@ -0,0 +1,6 @@
+module.exports = (req, res) => {
+    res.render('paisii', {
+        title: 'Паисий Хилендарски',
+        docTitle: 'Паисий Хилендарски'
+    });
+}
