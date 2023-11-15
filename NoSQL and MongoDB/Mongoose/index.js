8e0988fdf9169f7d6f9e175dc3ea13e80a960c31
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-15 17:37:37 +0200

    Add data to database

--- NoSQL and MongoDB/Mongoose/index.js
+++ NoSQL and MongoDB/Mongoose/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost:27017';
+const connectionString = 'mongodb://localhost:27017/testdb';
 
 start();
 
@@ -9,5 +9,20 @@ async function start(){
         useUnifiedTopology: true,
         useNewUrlParser: true
     })
+
+    const carShema = new mongoose.Schema({
+        name: String,
+        price: Number
+    })
+
+    const Car = mongoose.model('Car', carShema);
+    const car = new Car({
+        name: 'Opel Mokka',
+        price: 2500
+    })
+    await car.save();
+
+    const data = await Car.find({});
+    console.log(data);
     console.log('Connected');
 }
