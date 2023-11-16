56196fa76084f3e9a1a77cfeeca22a59a6de782e
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-16 07:41:47 +0200

    Add some changes

--- NoSQL and MongoDB/Mongoose-more-options/index.js
+++ NoSQL and MongoDB/Mongoose-more-options/index.js
@@ -3,35 +3,50 @@ const mongoose = require('mongoose');
 const connectionString = 'mongodb://localhost:27017/testdb';
 
 const carShema = new mongoose.Schema({
-    name: {type: String, required: true},
-    price:{type: Number, default: 0}
+    name: { type: String, required: true },
+    price: {
+        type: Number,
+        default: 0,
+        // validate: {
+        //     validator: function (value) {
+        //         return value >= 0;
+        //     },
+        //     message: 'Error'
+        // }
+        min: [0, 'Price cannot be less than {VALUE}'],
+    }
 })
 
-carShema.methods.startEngine = function(){
+carShema.methods.startEngine = function () {
     console.log(`${this.name} - ${this.price}`);
 }
-carShema.virtual('VAT').get(function(){
+carShema.virtual('VAT').get(function () {
     return this.price * 0.2;
 })
 const Car = mongoose.model('Car', carShema);
 
 start();
 
-async function start(){
+async function start() {
     await mongoose.connect(connectionString, {
         useUnifiedTopology: true,
         useNewUrlParser: true
     })
 
-    // const car = new Car({
-    //     name: 'Opel Mokka',
-    //     price: 2500
-    // })
-    // await car.save();
+    try {
+        const car = new Car({
+            name: 'Mercedes',
+            price: -2
+        })
+        await car.save();
+    } catch (err) {
+        console.log(err._message);
+    }
 
-    const data = await Car.find({});
-    console.log(data);
-    data.forEach(car => car.startEngine());
-    data.forEach(car => console.log(car.VAT));
+
+    // const data = await Car.find({});
+    // console.log(data);
+    // data.forEach(car => car.startEngine());
+    // data.forEach(car => console.log(car.VAT));
 
 }
