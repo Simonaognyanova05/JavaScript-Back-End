2b10b206f25e8f23e1d59072256cd4eecb0ea3b0
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-18 18:07:01 +0200

    Add reading and writing request to my-exercise

--- NoSQL and MongoDB/My-Exercise-With-Mongoose-Queries/index.js
+++ NoSQL and MongoDB/My-Exercise-With-Mongoose-Queries/index.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose');
-const Car = require('./models/Car');
-const connectionString = 'mongodb://localhost:27017/testdb';
+const Cat = require('./models/Cat');
+
+const connetcionString = 'mongodb://localhost:27017/testdb';
 
 start();
 
-async function start() {
-    await mongoose.connect(connectionString, {
+async function start(){
+    await mongoose.connect(connetcionString, {
         useUnifiedTopology: true,
         useNewUrlParser: true
     })
 
-    //Get data by name
-    // const car = await Car.find({}).select('name');
-    // console.log(car);
+    // try{
+    //     const cat = new Cat({
+    //         name: 'Maxi',
+    //         age: 0.5,
+    //     });
+    //     await cat.save();
+    // }catch(err){
+    //     console.log('Error');
+    // }
 
-    //Sort data by price by dec
-    // const car = await Car.find({}).sort({price: -1});
-    // console.log(car);
-
-    //Sort data by price by aec
-    const car = await Car.find({}).sort({price: 1});
-    console.log(car);
+    const cat = await Cat.findOne({name: 'Lussy'});
+    console.log(cat);
 }
