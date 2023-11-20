37d4aaf1c75515f8021407492a17fdc2cb0da9ce
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-20 20:28:47 +0200

    Add some things for new project

--- NoSQL and MongoDB/My-Exercise-With-Mongoose-Queries/index.js
+++ NoSQL and MongoDB/My-Exercise-With-Mongoose-Queries/index.js
@@ -1,33 +1,15 @@
-const mongoose = require('mongoose');
-const Cat = require('./models/Cat');
+const express = require('express');
+const hbs = require('express-handlebars');
+const homeController = require('./src/home');
 
-const connetcionString = 'mongodb://localhost:27017/testdb';
+const app = express();
+app.engine('.hbs', hbs.create({
+    extname: '.hbs'
+}).engine);
 
-start();
+app.set('view engine', '.hbs');
+app.get('/', homeController);
 
-async function start(){
-    await mongoose.connect(connetcionString, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    })
+app.listen(3000);
 
-    try{
-        const cat = new Cat({
-            name: 'Maxi',
-            age: 0.5,
-        });
-        await cat.save();
-    }catch(err){
-        console.log('Error');
-    }
 
-    // const cat = await Cat.findOne({name: 'Lussy'});
-    // console.log(cat);
-
-    // const cat = await Cat.findByIdAndUpdate('6558e0aecbe44d0e43fee447');
-    // cat.name = 'Lussy';
-    // await cat.save();
-
-    const cat = await Cat.findByIdAndDelete('6558e0aecbe44d0e43fee447');
-    await cat.save();
-}
