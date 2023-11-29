fec32319cbfe6b72128950f2b7bc81423bfe97ac
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-29 09:18:11 +0200

    Add demo-1

---
+++ Session and Authentication/demo-1/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+let visited = 0;
+
+app.get('/', (req, res) => {
+    if(req.headers.cookie){
+        console.log('Cookie ' + req.headers.cookie);
+        visited = Number(req.headers.cookie.split('=')[1]);
+        visited++;
+    }
+    res.setHeader('Set-Cookie', `visited=${visited}; httpOnly`);
+    res.send(`<p>Hello</p><p>You have visited ${visited} times</p>`)
+})
+app.listen(3000);
