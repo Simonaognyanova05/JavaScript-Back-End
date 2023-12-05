50d55260105fccff06b9c9c4beabfb6966f1d59d
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-12-05 11:22:10 +0200

    Add basic database

--- Workshop-Session-and-Authentication/cars/src/home.js
+++ Workshop-Session-and-Authentication/cars/src/home.js
@@ -1,5 +1,21 @@
+const cars = {
+    '123': {
+        brand: 'Mercedes',
+        model: 'C-Class',
+        year: '2020',
+        price: '75000',
+    },
+    '456': {
+        brand: 'Audi',
+        model: 'A8',
+        year: '2016',
+        price: '40000',
+    },
+}
+
 module.exports = (req, res) => {
     res.render('home', {
         title: 'Home page',
+        cars
     });
 }
