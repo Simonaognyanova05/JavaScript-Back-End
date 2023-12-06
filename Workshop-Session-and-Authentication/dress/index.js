27a4d615e2e6573afafa96b27a2fa83598865755
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-12-06 17:02:42 +0200

    Add new project

---
+++ Workshop-Session-and-Authentication/dress/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const homeController = require('./src/home');
+
+const app = express();
+app.engine('.hbs', hbs.create({
+    extname: '.hbs'
+}).engine);
+
+app.set('view engine', '.hbs');
+//app.use('/content', express.static('static'));
+
+app.get('/', homeController)
+app.listen(3000);
