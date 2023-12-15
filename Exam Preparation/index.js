88945e86f3bfcedf393dbadacf7e39eb5859c7c4
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-12-15 12:14:05 +0200

    Add home page

---
+++ Exam Preparation/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const expressSession = require('express-session');
+const hbs = require('express-handlebars');
+const homeController = require('./src/home');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(expressSession({
+    secret: 'secret cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: 'auto' }
+}));
+
+app.engine('.hbs', hbs.create({
+    extname: '.hbs'
+}).engine);
+
+app.set('view engine', '.hbs');
+app.use('/content', express.static('static'));
+
+app.get('/', homeController);
+
+app.listen(3000);
