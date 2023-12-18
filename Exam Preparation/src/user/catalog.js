d6aefb5678a6de13b2474383c445622c00bebb96
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-12-18 21:50:59 +0200

    Get data from database and make visible to user

--- Exam Preparation/src/user/catalog.js
+++ Exam Preparation/src/user/catalog.js
@@ -1,3 +1,5 @@
-module.exports = (req, res) => {
-    res.render('user/catalog', { title: 'Catalog Page' });
+const { getData } = require('./requests/getData');
+module.exports = async (req, res) => {
+    const cars = await getData();
+    res.render('user/catalog', { title: 'Catalog Page', cars: cars });
 };
---
+++ Exam Preparation/src/user/requests/getData.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Car = require('../../admin/requests/models/Car');
+
+const connectionString = 'mongodb://localhost:27017/cars';
+
+async function getData(req, res){
+    await mongoose.connect(connectionString, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+
+
+    try{
+        const cars = (await Car.find({})).map(car => car.toJSON());
+        return cars;
+       
+    }catch(err){
+        res.send('Error');
+    }
+}
+
+module.exports = {getData};
